f4832cf1061e0849d94d5350db7c8e20bb16c594
mulobbo <utku2.akturk@outlook.com>
2021-03-07 19:17:51 +0300

    +++

---
+++ utku-bank-db/src/main/java/net/utku/bank/db/model/Customer.java
@@ -0,0 +1,15 @@
+package net.utku.bank.db.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.utku.bank.db.model.enumaration.CustomerStatusType;
+
+@Getter @Setter @NoArgsConstructor
+public class Customer extends UserEntityBase {
+
+    private CustomerStatusType status;
+
+  // private List<Account> accounts = new ArrayList<>();
+
+}
